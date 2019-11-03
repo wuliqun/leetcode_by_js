bed6df5ce4215572f258fb6450d961a9229d3222
wuliqun <540744846@qq.com>
2019-11-03 22:51:54 +0800

    modify linkList test function

--- linkListTest.js
+++ linkListTest.js
@@ -26,15 +26,24 @@ function linkListToArray(head){
   }
   return res;
 }
-
+function transformArgs(args){
+  let len = args.length,i;
+  for(i = 0;i<len;i++){
+    // 在前面的数组参数会被转换成链表
+    if(Array.isArray(args[i])){
+      args[i] = formatLinkList(args[i]);
+    }else{
+      break;
+    }
+  }
+}
 /**
  * 测试方法
  * @param {*} fn 方法对象
- * @param {*} args 方法参数数组,args[0]为链表的数组形式
+ * @param {*} args 方法参数数组,args[0:i]若为数组,则转换成链表的
  */
 function testLinkList(fn,args){
-  let head = formatLinkList(args[0]);
-  args[0] = head;
+  transformArgs(args);
   let res =  fn.apply(null,args);
   return linkListToArray(res);
 }
